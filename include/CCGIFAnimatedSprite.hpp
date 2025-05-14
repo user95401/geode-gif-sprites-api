560ad7a545d293782e9e6324fdadaed07af82a3f
user95401 <90561697+user95401@users.noreply.github.com>
2025-05-14 20:42:15 +0400

    1.0.4 - optimizing and refining by @rodislav1337

--- include/CCGIFAnimatedSprite.hpp
+++ include/CCGIFAnimatedSprite.hpp
@@ -4,7 +4,7 @@
 #include <cocos2d.h>
 #include <Geode/utils/casts.hpp>
 
-//its only member reference and cast helper)
+//its only member reference and cast helper...
 class CCGIFAnimatedSprite : public cocos2d::CCSprite {
 public:
 
@@ -14,13 +14,17 @@ public:
         );
     }
 
+    // Frame textures
     const std::vector<cocos2d::CCTexture2D*>& getFrames() const { return m_frames; }
+    // Delays between frames
     const std::vector<float>& getDelays() const { return m_delays; }
+    // Current frame index
     size_t getCurrentIndex() const { return m_index; }
+    // Elapsed time since last frame
     float getElapsed() const { return m_elapsed; }
 
-    std::vector<cocos2d::CCTexture2D*> m_frames;
-    std::vector<float> m_delays;
-    size_t m_index;
-    float m_elapsed;
+    std::vector<cocos2d::CCTexture2D*> m_frames;     // Frame textures
+    std::vector<float> m_delays;            // Delays between frames
+    size_t m_index;                         // Current frame index
+    float m_elapsed;                        // Elapsed time since last frame
 };
